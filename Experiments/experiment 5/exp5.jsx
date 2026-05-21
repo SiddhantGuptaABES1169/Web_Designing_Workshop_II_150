0a583bf645b230f8476af3159d9c69eaec033f3c
SiddhantGuptaABES1169 <30807siddhant@gmail.com>
2026-05-22 00:00:22 +0530

    employee function start

---
+++ Experiments/experiment 5/exp5.jsx
@@ -0,0 +1,13 @@
+et employees = [];
+
+function addEmployee() {
+
+    let name = document.getElementById("name").value;
+    let id = document.getElementById("id").value;
+    let salary = parseFloat(document.getElementById("salary").value);
+    let department = document.getElementById("department").value;
+
+    if (name === "" || id === "" || isNaN(salary) || department === "") {
+        alert("Please fill all fields properly");
+        return;
+    }
